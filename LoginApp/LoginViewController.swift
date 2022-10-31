efa739563cd195b455b1b2e20ab885a2fefac20c
VladVelik <sosinvladik@yandex.ru>
2022-10-31 22:19:43 +0300

    done

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,21 +7,47 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     private let userName = "Vladislav"
     private let password = "password"
 
-    
-    @IBAction func forgotSomethingPressed(_ sender: Any) {
-        showMyAlert()
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userName
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
     
-    private func showMyAlert() {
-        let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: .alert)
+    @IBAction func logInPressed(_ sender: Any) {
+        if (userNameTextField.text == userName &&
+            passwordTextField.text == password) {
+            performSegue(withIdentifier: "identifier", sender: nil)
+        } else {
+            showMyAlert(title: "Hmm", message: "Incorrect Username or password!")
+            passwordTextField.text = ""
+            return
+        }
+    }
+    
+    @IBAction func unwind(unwindSegue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    @IBAction func forgotSomethingPressed(_ sender: Any) {
+        (sender as AnyObject).tag == 0
+            ? showMyAlert(title: "Hmmm", message: "Your name is \(userName)!")
+            : showMyAlert(title: "Hmmm", message: "Your password is \(password)!")
+    }
+    
+    private func showMyAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setGradient()
+        welcomeLabel.text = "Welcome, \(userName)!"
     }
-
-
+    
+    private func setGradient() {
+        let gradient = CAGradientLayer()
+        let blue = UIColor(red: 10/255, green: 91/255, blue: 133/255, alpha: 1)
+        let green = UIColor(red: 0, green: 146/255, blue: 139/255, alpha: 1)
+        gradient.colors = [blue.cgColor, green.cgColor, green.cgColor]
+        gradient.locations = [0, 0.3, 1]
+        gradient.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        view.layer.insertSublayer(gradient, at: 0)
+    }
 }
